0f7487c4fc7de05fd7f1efe4d7f39c69c8cd71c7
Corey Mays <corey.mays17@gmail.com>
2024-09-19 19:28:56 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        long numToExamine = 1111111111111111111L;
+        if (numToExamine % 2 == 0)
+        {
+            System.out.println("The number " + numToExamine + " is even");
+        }
+        else
+        {
+            System.out.println("The number " + numToExamine + " is odd");
+        }
     }
 }
